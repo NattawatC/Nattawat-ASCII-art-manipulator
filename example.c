3ede758944454cd02ff9146a25111134879d3f12
NattawatC <64011478@kmitl.ac.th>
2021-12-06 19:04:25 +0700

    using string.h

--- example.c
+++ example.c
@@ -1,10 +1,20 @@
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
 
-input = strtok(input_buffer, "\n ")
-while (input != NULL)
-{
-    printf("%s", input);
-    input = strtok(NULL, "\n");
+int main () {
+   char str[80] = "### ###";
+   const char s[2] = " ";
+   char *token;
+   
+   /* get the first token */
+   token = strtok(str, s);
+   
+   /* walk through other tokens */
+   while( token != NULL ) {
+      printf( " %s\n", token );
+    
+      token = strtok(NULL, s);
+   }
+   
+   return(0);
 }
-printf("\n");
---
+++ string.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    FILE *input = fopen("char.txt","r");
+    FILE *input2 = fopen("char2.txt","r");
+    char line1[50],line2[50];
+    char *n1,*n2;
+    char *token, *token2;
+    const char s[] = "\n";
+    if((input == NULL)||(input2 == NULL)){
+        printf("ERROR");
+        exit(1);
+    }
+    while(1){
+        n1 = fgets(line1, sizeof(line1), input);
+        n2 = fgets(line2, sizeof(line2), input2);
+        if((n1 == NULL) && (n2 == NULL)){
+            break;
+        }
+        token = strtok(n1, s);
+        token2 = strtok(n2, s);
+        strcat(token, token2);
+        puts(token);
+        puts(token2);
+    }
+    fclose(input);
+    fclose(input2);
+    return 0;
+}
