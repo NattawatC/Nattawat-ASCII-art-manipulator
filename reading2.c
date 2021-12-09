4faeb89c52fcad8c2d78b959d07d976761dedf89
NattawatC <64011478@kmitl.ac.th>
2021-12-09 23:31:25 +0700

    free space
    using malloc, still not working. need to get rid of \n and NULL.

--- reading2.c
+++ reading2.c
@@ -3,30 +3,59 @@
 #include <string.h>
 
 int main(){
-    FILE *input = fopen("char.txt","r");
-    FILE *input2 = fopen("char2.txt","r");
+    FILE *input = fopen("C:\\Users\\USER\\Desktop\\input.txt","r");
+    FILE *input2 = fopen("C:\\Users\\USER\\Desktop\\after.txt","r");
     char line1[50],line2[50];
     char *n1,*n2;
+    char *ptr, *ptr2;
     if((input == NULL)||(input2 == NULL)){
         printf("ERROR");
         exit(1);
     }
+    fseek(input, 0, SEEK_END);
+    int size = ftell(input);
+//    printf("%d", size);
+    fseek(input, 0, SEEK_SET);
+
+    fseek(input2, 0L, SEEK_END);
+    int size2 = ftell(input2);
+//    printf("%d", size2);
+    fseek(input2, 0, SEEK_SET);
+
+    ptr = (char*)malloc(size);
+    ptr2 = (char*)malloc(size2);
+
     while(1){
         n1 = fgets(line1, sizeof(line1), input);
         n2 = fgets(line2, sizeof(line2), input2);
         if((n1 == NULL) && (n2 == NULL)){
             break;
         }
-        int len = strlen(line1);
-        if (len > 0 && line1[len - 1] == '\n'){
-            line1[len - 1] = 0;
-        }
-        if (n1 == NULL){
-            *n1 = '\0';
-        }
-        printf("%s", n1);
-        printf("%s", n2);
+        strncpy(ptr, n1, strlen(n1));
+        ptr[strlen(n1) - 1] = '\0';
+//        strncpy(ptr2, n2, strlen(n2));
+//        ptr[strlen(n2) - 1] = '\0';
+//        ptr2 = n2;
+//        if (*ptr == '\n'){
+//            ptr = 0;
+//        }
+        puts(ptr);
+//        puts(ptr2);
+
+
+//        int len = strlen(line1);
+//        if (len > 0 && line1[len - 1] == '\n'){
+//            line1[len - 1] = 0;
+//        }
+
+//         if (n1 == NULL){
+//             *n1 = '\0';
+//         }
+//        printf("%s", n1);
+//        printf("%s", n2);
     }
+    free(ptr);
+    free(ptr2);
     fclose(input);
     fclose(input2);
     return 0;
--- string.c
+++ string.c
@@ -13,19 +13,20 @@ int main(){
         printf("ERROR");
         exit(1);
     }
-    while(1){
-        n1 = fgets(line1, sizeof(line1), input);
-        n2 = fgets(line2, sizeof(line2), input2);
-        if((n1 == NULL) && (n2 == NULL)){
-            break;
-        }
-        token = strtok(n1, s);
-        token2 = strtok(n2, s);
-        strcat(token, token2);
-        puts(token);
-        puts(token2);
-    }
-    fclose(input);
-    fclose(input2);
+    fseek(input, 0, SEEK_SET);
+    // while(1){
+    //     n1 = fgets(line1, sizeof(line1), input);
+    //     n2 = fgets(line2, sizeof(line2), input2);
+    //     if((n1 == NULL) && (n2 == NULL)){
+    //         break;
+    //     }
+    //     token = strtok(n1, s);
+    //     token2 = strtok(n2, s);
+    //     strcat(token, token2);
+    //     puts(token);
+    //     // puts(token2);
+    // }
+    // fclose(input);
+    // fclose(input2);
     return 0;
 }
