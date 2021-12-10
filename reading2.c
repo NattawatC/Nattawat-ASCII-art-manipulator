ac25060f1d2b98bcf21b6b84df3065cc606d8f7d
NattawatC <64011478@kmitl.ac.th>
2021-12-10 16:50:16 +0700

    string.h

--- reading2.c
+++ reading2.c
@@ -7,9 +7,12 @@ int main(){
     FILE *input2 = fopen("C:\\Users\\USER\\Desktop\\after.txt","r");
     char line1[50],line2[50];
     char *n1,*n2;
-    char *ptr, *ptr2;
+
+    const char s[] = "\n";
+    char *token, *token2;
+
     if((input == NULL)||(input2 == NULL)){
-        printf("ERROR");
+        printf("Can't find a File");
         exit(1);
     }
     fseek(input, 0, SEEK_END);
@@ -17,13 +20,13 @@ int main(){
 //    printf("%d", size);
     fseek(input, 0, SEEK_SET);
 
-    fseek(input2, 0L, SEEK_END);
+    fseek(input2, 0, SEEK_END);
     int size2 = ftell(input2);
 //    printf("%d", size2);
     fseek(input2, 0, SEEK_SET);
 
-    ptr = (char*)malloc(size);
-    ptr2 = (char*)malloc(size2);
+    token = (char*)malloc(size);
+    token2 = (char*)malloc(size2);
 
     while(1){
         n1 = fgets(line1, sizeof(line1), input);
@@ -31,31 +34,30 @@ int main(){
         if((n1 == NULL) && (n2 == NULL)){
             break;
         }
-        strncpy(ptr, n1, strlen(n1));
-        ptr[strlen(n1) - 1] = '\0';
-//        strncpy(ptr2, n2, strlen(n2));
-//        ptr[strlen(n2) - 1] = '\0';
-//        ptr2 = n2;
-//        if (*ptr == '\n'){
-//            ptr = 0;
-//        }
-        puts(ptr);
-//        puts(ptr2);
 
+        token = strtok(n1, s);
+        // ptr = n1;
+        for(int i = 0; i < strlen(n1); i++){
+            token[strlen(n1) - (i - 1)] = '\0';
+        }
+        
+        token2 = strtok(n2, s);
+        // ptr2 = n2;
+        for(int i = 0; i < strlen(n2); i++){
+            token2[strlen(n2) - (i - 2)] = '\0';
+        }
 
+        strcat(token, token2);
+        puts(token);
+        // puts(token2);
+        // printf("%s%s\n", ptr, ptr2);
 //        int len = strlen(line1);
 //        if (len > 0 && line1[len - 1] == '\n'){
 //            line1[len - 1] = 0;
 //        }
-
-//         if (n1 == NULL){
-//             *n1 = '\0';
-//         }
-//        printf("%s", n1);
-//        printf("%s", n2);
     }
-    free(ptr);
-    free(ptr2);
+    free(token);
+    free(token2);
     fclose(input);
     fclose(input2);
     return 0;
