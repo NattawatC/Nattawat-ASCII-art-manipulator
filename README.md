2056fe7675da0795527471131e07bb9a1e56b0b6
NattawatC <64011478@kmitl.ac.th>
2021-12-13 15:47:54 +0700

    update

---
+++ README.md
@@ -0,0 +1,4 @@
+Individual C project 2021
+
+ASCII ART Manipulator:
+    merging character from 2 file using pointer.
--- merging.c
+++ merging.c
@@ -1,21 +1,61 @@
 #include <stdio.h>
- 
-int main () {
+#include <stdlib.h>
+#include <string.h>
 
-   /* an array with 5 rows and 2 columns*/
-   int a[5][2] = { {0,0}, {1,2}, {2,4}, {3,6},{4,8}};
-   int i, j;
- 
-   /* output each array element's value */
-   for ( i = 0; i < 5; i++ ) {
+int main(){
+    FILE *input = fopen("char.txt","r");        //read file one
+    FILE *input2 = fopen("char2.txt","r");      //read file two
+    FILE *output = NULL;
+    char *ptr, *ptr2;
 
-      for ( j = 0; j < 2; j++ ) {
-         printf("%d ", a[i][j]);
-		 if (j == 1){
-			 printf("\n");
-		 }
-      }
-   }
-   
-   return 0;
+    if((input == NULL)||(input2 == NULL)){      // check for the end of file
+        printf("Can't find a File");
+        exit(1);
+    }
+
+    fseek(input, 0, SEEK_END);      //set the end of file
+    int size = ftell(input);    //find the size of file(input)
+    fseek(input, 0, SEEK_SET);      //set to beginning of the file
+
+    fseek(input2, 0, SEEK_END);
+    int size2 = ftell(input2);    //find the size of file(input2)
+    fseek(input2, 0, SEEK_SET);
+
+    int size_t = size + size2;      //total file's size
+
+    ptr = (char*)malloc(size);
+    ptr2 = (char*)malloc(size2);
+
+    int arr1 = fread(ptr, 1, size, input);      //put char into pointer(ptr)
+    int arr2 = fread(ptr2, 1, size2, input2);       //put char2 into pointer(ptr2)
+    if((arr1 != size) || (arr2 != size2)){      //error checking
+        printf("ERROR");
+    }
+
+    char *ptr3;
+    ptr3 = (char*)malloc(size_t);
+    memcpy(ptr3, ptr, size_t);      //copy content ptr into ptr3
+
+    char *ptr4;
+    ptr4 = (char*)malloc(size_t);
+    memcpy(ptr4, ptr2, size_t);      //copy content ptr2 into ptr3 + 10(new_dest)
+
+//<still need to be fix>
+    output = fopen("Final_char.txt", "w");     //new file to write
+    fwrite(ptr3, 1, 2, output);     //putting fisrt 2 char
+    fwrite(ptr4, 1, 3, output);     //putting seconmd char
+    fwrite(ptr3 + 2, 1, 3, output);
+    fwrite(ptr4, 1, 4, output);
+    fprintf(output, "  ");
+    fwrite(ptr4, 1, 3, output);
+
+// done:
+    free(ptr);      //free pointer
+    free(ptr2);
+    free(ptr3);
+    free(ptr4);
+    fclose(input);      //close file
+    fclose(input2);
+    fclose(output);
+    return 0;
 }
